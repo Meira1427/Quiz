cb18851e6d338cacd6bc1eeef5c2cb5c6d3f9060
Meira Pentermann <meira@meirapentermann.com>
2017-10-17 14:20:16 -0600

    Add entity for Quiz

---
+++ JPAQuiz/src/entities/Quiz.java
@@ -0,0 +1,34 @@
+package entities;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Quiz {
+
+	private int id;
+	private String name;
+
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Quiz [id=");
+		builder.append(id);
+		builder.append(", name=");
+		builder.append(name);
+		builder.append("]");
+		return builder.toString();
+	}
+
+}
